53422b899b903541803d8d88e4bd09b3d546fc84
Bryan-Elliott Tam <bryanelliott.tam@ugent.be>
2023-06-27 14:41:35 +0200

    construction of the simple fragmentation

--- src/fragment.rs
+++ src/fragment.rs
@@ -12,13 +12,13 @@ pub struct Fragment {
 }
 
 impl Fragment {
-    pub fn new(filename: &PathBuf) -> Self {
+    pub fn new(filename: &PathBuf, max_size_cache: usize) -> Self {
         fs::File::create(filename).unwrap();
         Self {
             filename: filename.clone(),
             boundary: Boundary::default(),
             size: 0usize,
-            members_to_materialized: Vec::new(),
+            members_to_materialized: Vec::with_capacity(max_size_cache),
         }
     }
 
@@ -53,5 +53,27 @@ impl Default for Boundary {
             up: chrono::NaiveDate::MAX,
             down: chrono::NaiveDate::MIN,
         }
+    }
+}
+
+pub struct SimpleFragmentation {
+    fragments: Vec<Fragment>,
+}
+
+impl SimpleFragmentation {
+    pub fn new(n_fragments: usize, max_size_cache: usize, folder: &PathBuf) -> Self {
+        let fragments = {
+            let mut resp: Vec<Fragment> = Vec::with_capacity(n_fragments);
+            for i in 1..n_fragments {
+                let fragment_path = {
+                    let mut resp = folder.clone();
+                    resp.push(i.to_string());
+                    resp
+                };
+                resp.push(Fragment::new(&fragment_path, max_size_cache))
+            }
+            resp
+        };
+        Self { fragments }
     }
 }
