1583fd07fbd8a0fd9f247902008c4a2bdd3a3791
Bryan-Elliott Tam <bryanelliott.tam@ugent.be>
2023-08-08 15:33:33 +0200

    documentation added to the config

--- src/config.rs
+++ src/config.rs
@@ -4,14 +4,22 @@ use std::collections::HashSet;
 use std::fs::read_to_string;
 use std::path::PathBuf;
 
+/// Configuration of the parser, the member IRI must respect a regex expression
 #[derive(serde::Deserialize, Debug, Clone)]
 pub struct Config {
+    /// A regex that the IRI of the members must respect
     pub member_url_regex: String,
+    /// The [Schema]s of the member property
     pub schema: Vec<Schema>,
+    /// The number of members, those not have any impact on the execution it is for being shown to the user
     pub n_members: usize,
+    /// The date field for the fragmentation
     pub date_field: String,
+    /// The highest date present in the data dump
     pub highest_date: chrono::NaiveDateTime,
+    /// The lowest date present in the data dump
     pub lowest_date: chrono::NaiveDateTime,
+    /// The address of the server that will host the TREE document
     pub server_address: String,
 }
 impl Config {
@@ -22,6 +30,7 @@ impl Config {
     }
 }
 
+/// The schema of a triple pattern associated with a TREE member
 #[derive(serde::Deserialize, Debug, Clone)]
 pub struct Schema {
     pub subject: SubjectDescriptor,
@@ -29,6 +38,7 @@ pub struct Schema {
     pub object: Option<SubjectDescriptor>,
 }
 impl Schema {
+    /// check if a triple is valid with the schema
     pub fn is_valid(&self, input: &SchemaValidatorInput) -> bool {
         match &self.subject {
             SubjectDescriptor::MemberSubject => {
@@ -50,12 +60,17 @@ impl Schema {
         return true;
     }
 }
+
+/// A descriptor for a subject in the context of a triple
 #[derive(serde::Deserialize, Debug, Clone)]
 pub enum SubjectDescriptor {
+    /// The subject is the member
     MemberSubject,
+    /// The subject is a property or subproperty of the member
     LinkedSubject { subject: String },
 }
 
+/// Input argument of the [Schema] [Schema::is_valid]
 pub struct SchemaValidatorInput {
     pub subject: String,
     pub predicate: String,
